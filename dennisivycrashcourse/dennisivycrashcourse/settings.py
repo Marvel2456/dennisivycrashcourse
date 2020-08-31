8b5c3c0c4ea06a253653e29385fc3b85cd4d1ee1
lisyce <lisyce@users.noreply.github.com>
2020-08-31 11:29:37 -0700

    Missed changes lecture 4

--- dennisivycrashcourse/dennisivycrashcourse/settings.py
+++ dennisivycrashcourse/dennisivycrashcourse/settings.py
@@ -119,3 +119,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
