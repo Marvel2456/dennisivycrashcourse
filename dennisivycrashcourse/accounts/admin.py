1ef38063de8ff9cdea3fb47c6da0bc4e6d41cfd6
lisyce <lisyce@users.noreply.github.com>
2020-09-01 20:27:18 -0700

    Lecture 6- Database Relationships

--- dennisivycrashcourse/accounts/admin.py
+++ dennisivycrashcourse/accounts/admin.py
@@ -5,4 +5,5 @@ from .models import *
 
 admin.site.register(Customer)
 admin.site.register(Product)
+admin.site.register(Tag)
 admin.site.register(Order)
--- dennisivycrashcourse/accounts/models.py
+++ dennisivycrashcourse/accounts/models.py
@@ -5,10 +5,17 @@ class Customer(models.Model):
     name = models.CharField(max_length=200, null=True)
     phone = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200, null=True)
-    date_created = models.DateTimeField(auto_now_add=True);
+    date_created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+
+class Tag(models.Model):
+    name = models.CharField(max_length=200, null=True)
+
+    def __str__(self):
+        return self.name
+
 
 class Product(models.Model):
     CATEGORY = (
@@ -18,8 +25,12 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=200, null=True, choices=CATEGORY)
-    description =models.CharField(max_length=200, null=True)
-    date_created = models.DateTimeField(auto_now_add=True);
+    description =models.CharField(max_length=200, null=True, blank=True)
+    date_created = models.DateTimeField(auto_now_add=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 class Order(models.Model):
     STATUS = (
@@ -28,7 +39,7 @@ class Order(models.Model):
         ('Delivered', 'Delivered')
     )
 
-    #customer =
-    #product =
+    customer = models.ForeignKey(Customer, null=True, on_delete=models.SET_NULL)
+    product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True)
     status = models.CharField(max_length=200, null=True, choices=STATUS)
